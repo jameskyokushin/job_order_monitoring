93bc0524655b055b64f7bfbffd6c62816f236e8e
jameskyokushin <james@kyokushin.com.ph>
2011-09-05 16:35:07 +0800

    done with the devise

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobOrder001::Application.config.secret_token = 'eb13d5d1040a2ff2847dfe3904c27245fc22d8b1c0b61f3cff7f06ee55c64257b01e948936a386bedbc54aa6db1f3d693b502a979884fe1a0366b6d4e325c2d5'
