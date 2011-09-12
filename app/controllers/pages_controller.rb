6121ad9011bf4604931270bad0e644f24202ed39
jameskyokushin <james@kyokushin.com.ph>
2011-09-12 12:10:16 +0800

    edit README

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,7 @@ class PagesController < ApplicationController
   def accomplished
     @job = Job.find params[:id]
     @job.update_attributes(:done => true)
-    redirect_to accomplished_path(job)
+    redirect_to accomplished_path(@job)
   end 
   
   def current_user
