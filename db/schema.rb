679800937062d1ddad773d0bb35301ecd5f5ffc8
jameskyokushin <james@kyokushin.com.ph>
2011-09-16 02:15:59 +0800

    migrate jobdate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110915154215) do
+ActiveRecord::Schema.define(:version => 20110915181343) do
 
   create_table "inquiries", :force => true do |t|
     t.date     "date"
